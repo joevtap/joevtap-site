d3124e7908b7811da9aef764189afd19cb35bf02
joevtap <joelvitortorres@gmail.com>
2024-01-28 16:29:58 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    compress(),
-  ],
+  integrations: [tailwind(), compress()],
 });
